9a93fd1a2e4e540fc91595e0307301fdf3a00d71
huige <yagaoxx@hotmail.com>
2026-08-21 14:13:19 +0800

    Add files via upload

---
+++ static/js/statistics.js
@@ -0,0 +1,7 @@
+﻿var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5463a0b72d3d5c171b2819c2a176de96";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
